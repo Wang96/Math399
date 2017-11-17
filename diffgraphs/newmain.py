2ca3b8ff843cb418d073f619b04cfef96646c81f
Wang96 <nwang@colgate.edu>
2017-11-17 08:50:25 -0500

    Generalize

--- diffgraphs/newmain.py
+++ diffgraphs/newmain.py
@@ -1,21 +1,29 @@
+import numpy as np
+import networkx as nx
+
 import newsolver1 as ode
 import network as nt
-import numpy as np
+
 
 def vanderpol(Y,p):
     return [Y[1],p[1]*(1-Y[0]**2)*Y[1]-Y[0]]
 
+def harmonic(Y,p):
+    return [Y[1],-p[1]*Y[0]]
 
 def buildGraph():
     return nt.construct()
 
 
-def solve(equation,graph,coupling_term):
-    ode.simulate(equation,graph,coupling_term)
+def solve(equation,param,time,graph,coupling_term):
+    return ode.simulate(equation,param,time,graph,coupling_term)
 
 
 def main():
-    graph = buildGraph()
-    solve(vanderpol,graph,np.matrix([[0,0],[1,0]]))
+    #graph = buildGraph()
+    graph = nx.binomial_graph(10,0.1)
+    (t,sol) = solve(harmonic,[2,2],np.linspace(0, 100, 500),graph,np.matrix([[0,0],[1,0]]))
+    return (t,sol)
 
-main()
+(t,sol) = main()
+
